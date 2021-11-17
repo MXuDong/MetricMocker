bad0af5adad89de2e8939821859fa281b9413d77
MXuDong <1586793553@qq.com>
2021-11-17 17:56:06 +0800

    add functions

--- pkg/funcs/funcs.go
+++ pkg/funcs/funcs.go
@@ -5,7 +5,40 @@ package funcs
 //
 // For input is type of int64, but return is float64, so it will lose some precision.
 type Function interface {
+	// Init the function
+	Init(map[string]float64)
 
 	// Execute return res of input x by function
 	Execute(x int64) float64
+}
+
+// ResetAble mean the function can be reset,(some function need quick cal, need store some result, invoke it to clear)
+type ResetAble interface {
+	Reset()
+}
+
+const (
+	Slope   = "slope"
+	OffsetX = "offsetX"
+	OffsetY = "offsetY"
+)
+
+// GetParam return the param value, if not exits, return default value
+func GetParam(name string, param map[string]float64, defaultValue float64) float64 {
+	if value, ok := param[name]; ok {
+		return value
+	}
+	return defaultValue
+}
+
+// ZeroFunction always return zero
+type ZeroFunction struct {
+}
+
+func (z ZeroFunction) Init(m map[string]float64) {
+	// do nothing
+}
+
+func (z ZeroFunction) Execute(x int64) float64 {
+	return 0
 }
--- pkg/funcs/linear_function.go
+++ pkg/funcs/linear_function.go
@@ -1,38 +1,60 @@
 package funcs
 
 // LinearFunction is the simple linear-function.
-// the func is : y = slope * x + offset
+// the func is : y = slope * (x + offsetX) + offsetY
 type LinearFunction struct {
-	slope  float64
-	offset float64
+	slope   float64
+	offsetX float64
+	offsetY float64
 }
 
 func (l *LinearFunction) Execute(x int64) float64 {
-	return l.slope*float64(x) + l.offset
+	return l.slope*(float64(x)+l.offsetX) + l.offsetY
+}
+
+// Init linear function, need slope, offsetX and offsetY
+func (l *LinearFunction) Init(param map[string]float64) {
+	l.slope = GetParam(Slope, param, 0)
+	l.offsetX = GetParam(OffsetX, param, 0)
+	l.offsetY = GetParam(OffsetY, param, 0)
+}
+
+func NewLinearFunctionByMap(param map[string]float64) Function {
+	l := LinearFunction{}
+	l.Init(param)
+	return &l
 }
 
 // NewLinearFunction return new linear-function of special slope and offset.
-func NewLinearFunction(slope, offset float64) Function {
-	return &LinearFunction{
-		slope:  slope,
-		offset: offset,
+func NewLinearFunction(slope, offsetX, offsetY float64) Function {
+	l := LinearFunction{}
+	initParam := map[string]float64{
+		Slope:   slope,
+		OffsetX: offsetX,
+		OffsetY: offsetY,
 	}
-}
-
-// DefaultLinearFunction return function with expression is : y = x
-func DefaultLinearFunction() Function {
-	return NewLinearFunction(1, 0)
-}
-
-// ReverseLinearFunction return function with expression is : y = -x
-func ReverseLinearFunction() Function {
-	return NewLinearFunction(-1, 0)
+	l.Init(initParam)
+	return &l
 }
 
 func (l *LinearFunction) Slope() float64 {
 	return l.slope
 }
 
-func (l *LinearFunction) Offset() float64 {
-	return l.offset
+func (l *LinearFunction) OffsetX() float64 {
+	return l.offsetX
+}
+
+func (l *LinearFunction) OffsetY() float64 {
+	return l.offsetY
+}
+
+// DefaultLinearFunction return function with expression is : y = x
+func DefaultLinearFunction() Function {
+	return NewLinearFunction(1, 0, 0)
+}
+
+// ReverseLinearFunction return function with expression is : y = -x
+func ReverseLinearFunction() Function {
+	return NewLinearFunction(-1, 0, 0)
 }
